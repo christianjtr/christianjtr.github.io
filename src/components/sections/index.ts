b3861bd00c1509845d3c4a820c50e155a46da4bb
Christian José Torrealba Rondón <torrealba.christian@gmail.com>
2025-08-19 19:40:32 +0200

    feat: Split page into dedicated sections

---
+++ src/components/sections/index.ts
@@ -0,0 +1,3 @@
+export { default as Projects } from './Projects.astro';
+export { default as ProfessionalExperiences } from './ProfessionalExperiences.astro';
+export { default as Studies } from './Studies.astro';
