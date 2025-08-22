216ca9fa128f8d942d4521b3f99d1bcb5dbb4ecb
Christian José Torrealba Rondón <torrealba.christian@gmail.com>
2025-08-22 15:43:21 +0200

    feat: Add astro components and views

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,1 +1,3 @@
+export { default as BadgeList } from './BadgeList.astro';
+export { default as ExternalLinkButton } from './ExternalLinkButton.astro';
 export { default as Section } from './Section.astro';
--- src/components/sections/index.ts
+++ src/components/sections/index.ts
@@ -1,3 +1,4 @@
+export { default as About } from './About.astro';
 export { default as Projects } from './Projects.astro';
 export { default as ProfessionalExperiences } from './ProfessionalExperiences.astro';
 export { default as Studies } from './Studies.astro';
