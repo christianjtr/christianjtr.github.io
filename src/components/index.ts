5397ceb7ff8fb8cb9f41db7732cd0c09a31bdf71
Christian José Torrealba Rondón <torrealba.christian@gmail.com>
2025-09-01 16:21:35 +0200

    feat: Adjust components

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,3 +1,3 @@
 export { default as BadgeList } from './BadgeList.astro';
-export { default as ExternalLinkButton } from './ExternalLinkButton.astro';
+export { default as LinkButton } from './LinkButton.astro';
 export { default as Section } from './Section.astro';
