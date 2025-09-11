a28cba8460f8c0b2fafcea9e0dadf19df4dcf9d4
Christian José Torrealba Rondón <torrealba.christian@gmail.com>
2025-09-11 22:54:05 +0200

    feat: Add new layout component - horizontal spacer

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,3 +1,4 @@
 export { default as BadgeList } from './BadgeList.astro';
+export { default as HorizontalSpacer } from './HorizontalSpacer.astro';
 export { default as LinkButton } from './LinkButton.astro';
 export { default as Section } from './Section.astro';
