8e87fba6cfc957acc7555a1d6734432696ebdd4d
Christian José Torrealba Rondón <torrealba.christian@gmail.com>
2025-09-11 17:00:57 +0200

    feat: Add role experiment icons

--- src/schemas/ExperimentSchema.ts
+++ src/schemas/ExperimentSchema.ts
@@ -1,12 +1,19 @@
 import { z } from 'astro:content';
 
+enum EXPERIMENT_ROLE {
+    AUTHOR = 'Author',
+    CONTRIBUTOR = 'Contributor'
+}
+
+export type ExperimentRole = `${EXPERIMENT_ROLE}`;
+
 export const ExperimentSchema = z.object({
     id: z.string(),
     title: z.string(),
     description: z.string(),
     image: z.string(),
     tags: z.array(z.string()),
-    role: z.string(),
+    role: z.nativeEnum(EXPERIMENT_ROLE),
     category: z.string(),
     type: z.string(),
     url: z.string().url().nullable(),
