8d1e4a5f9c3a9e433a1e37975566502823c832c5
Christian José Torrealba Rondón <torrealba.christian@gmail.com>
2025-10-08 20:52:18 +0200

    feat: Add credential subsection

--- src/schemas/CredentialSchema.ts
+++ src/schemas/CredentialSchema.ts
@@ -1,17 +1,31 @@
 import { z } from 'astro:content';
 
-export const CertificationSchema = z.object({
+const iconKeys = ["book", "pallete", "code"] as const;
+
+const CredlyCredentialSchema = z.object({
+    id: z.string(),
+    name: z.string(),
+    image_url: z.string().url(),
+    organization: z.string(),
+    badge_url: z.string().url(),
+    date: z.string(),
+});
+
+const CertificationSchema = z.object({
     id: z.string(),
     title: z.string(),
     date: z.string(),
+    organization: z.string(),
     url: z.string().url(),
+    icon_key: z.enum(iconKeys),
     code: z.string().nullable(),
     display: z.boolean(),
 });
 
 export const CrendentialSchema = z.object({
-    credly_credentials: z.array(z.string()),
+    credly_credentials: z.array(CredlyCredentialSchema),
     certifications: z.array(CertificationSchema),
 });
 
+export type IconKey = typeof iconKeys[number];
 export type Credentials = z.infer<typeof CrendentialSchema>;
