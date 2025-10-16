567b18e6add37dda675d4b57f85bbcd79e96c121
Christian José Torrealba Rondón <torrealba.christian@gmail.com>
2025-10-16 21:23:57 +0200

    chore: Adjust component

--- src/components/index.ts
+++ src/components/index.ts
@@ -3,3 +3,4 @@ export { default as BadgeList } from './BadgeList.astro';
 export { default as HorizontalSpacer } from './HorizontalSpacer.astro';
 export { default as LinkButton } from './LinkButton.astro';
 export { default as Section } from './Section.astro';
+export { default as ExternalLinkIcon } from './ExternalLinkIcon.astro';
