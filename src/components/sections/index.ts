a579bca386e3c7eaa7fc7c21e87a20db8730da1e
Christian José Torrealba Rondón <torrealba.christian@gmail.com>
2025-10-24 15:24:50 +0200

    chore: Remove unnecesary section

--- src/components/sections/index.ts
+++ src/components/sections/index.ts
@@ -3,4 +3,3 @@ export { default as Credentials } from './Credentials.astro';
 export { default as Experiments } from './Experiments.astro';
 export { default as Projects } from './Projects.astro';
 export { default as ProfessionalExperiences } from './ProfessionalExperiences.astro';
-export { default as Studies } from './Studies.astro';
