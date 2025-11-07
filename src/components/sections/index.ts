a0a2a648d0a69f3e1a6dbc8f43fa3b6f969d63bd
Christian José Torrealba Rondón <torrealba.christian@gmail.com>
2025-11-07 18:32:14 +0100

    feat: :sparkles: Add opengraph component - first implementation

--- src/components/sections/index.ts
+++ src/components/sections/index.ts
@@ -3,3 +3,4 @@ export { default as Credentials } from './Credentials.astro';
 export { default as Experiments } from './Experiments.astro';
 export { default as Projects } from './Projects.astro';
 export { default as ProfessionalExperiences } from './ProfessionalExperiences.astro';
+export { default as OpenGraph } from './OpenGraph.astro';
