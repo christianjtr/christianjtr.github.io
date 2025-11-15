b64344bfa24d4b601d19aee3fbeec49eb3cbc23c
Christian José Torrealba Rondón <torrealba.christian@gmail.com>
2025-11-15 22:58:27 +0100

    chore: Change link to external file

--- src/utils/rrss.ts
+++ src/utils/rrss.ts
@@ -3,7 +3,7 @@ type LinkIcon = "linkedin" | "github" | "llms";
 const rrssLinks: Array<{ id: LinkIcon, text: string; url: string }> = [
     { id: "linkedin", text: "LinkedIn", url: "https://www.linkedin.com/in/christianjtr/" },
     { id: "github", text: "GitHub", url: "https://github.com/christianjtr" },
-    { id: "llms", text: "LLMS.txt", url: "/src/assets/llms.txt" }
+    { id: "llms", text: "LLMS.txt", url: "/llms.txt" }
 ];
 
 export { rrssLinks };
