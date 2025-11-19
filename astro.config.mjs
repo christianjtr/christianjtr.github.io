58b073a453ed718fca823c0c827b5421858ce378
Christian José Torrealba Rondón <torrealba.christian@gmail.com>
2025-11-19 20:22:27 +0100

    chore: Add API version variable

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,15 @@
 import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 import { i18nLocalesConfig } from './i18n.config.mjs';
+import packageJson from './package.json';
 
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
+    define: {
+      '__APP_VERSION__': JSON.stringify(packageJson.version),
+    }
   },
   i18n: i18nLocalesConfig,
   site: 'https://christianjtr.github.io',
