7528ce6536e745934c7c2b336e4859a89e405030
Christian José Torrealba Rondón <13179028+christianjtr@users.noreply.github.com>
2026-04-01 14:10:05 +0200

    chore: :wrench: Update configuration

--- i18n.config.mjs
+++ i18n.config.mjs
@@ -2,7 +2,7 @@ export const i18nLocalesConfig = {
   locales: ["es", "en", "fr"],
   defaultLocale: "en",
   routing: {
-    prefixDefaultLocale: false,
+    prefixDefaultLocale: true,
     redirectToDefaultLocale: true
   }
 };
