c950192a26fd0033366c3c4844cc2a3c56465878
Christian José Torrealba Rondón <13179028+christianjtr@users.noreply.github.com>
2026-07-06 11:07:16 +0200

    chore: Include alias

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,26 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 import { i18nLocalesConfig } from './i18n.config.mjs';
 import packageJson from './package.json';
+import path from 'path';
+import { fileURLToPath } from 'url';
 
-// https://astro.build/config
+const srcDir = fileURLToPath(new URL('./src', import.meta.url));
+
 export default defineConfig({
   vite: {
+    resolve: {
+      alias: {
+        '@assets': path.resolve(srcDir, 'assets'),
+        '@content': path.resolve(srcDir, 'content'),
+        '@components': path.resolve(srcDir, 'components'),
+        '@layouts': path.resolve(srcDir, 'layouts'),
+        '@styles': path.resolve(srcDir, 'styles'),
+        '@schemas': path.resolve(srcDir, 'schemas'),
+        '@app-types': path.resolve(srcDir, 'types'),
+        '@utils': path.resolve(srcDir, 'utils'),
+      },
+    },
     plugins: [tailwindcss()],
     define: {
       '__APP_VERSION__': JSON.stringify(packageJson.version),
