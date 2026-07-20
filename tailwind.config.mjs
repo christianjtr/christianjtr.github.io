7a09ccaae0e4036164514646b3c12db9a299fdb6
Christian José Torrealba Rondón <13179028+christianjtr@users.noreply.github.com>
2026-07-20 13:37:22 +0200

    chore(deps): remove unused packages and legacy Tailwind integration

--- tailwind.config.mjs
+++
@@ -1,9 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-export default {
-  content: [
-    './src/**/*.{astro,html,svelte,vue,js,ts,jsx,tsx}'
-  ],
-  plugins: [
-    require('@tailwindcss/typography'),
-  ],
-}
